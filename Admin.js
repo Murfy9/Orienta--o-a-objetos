536d3b35fe7ce79b289b03b0ff3ddf0aeca8bd4e
Pedro Xavier <pedroxxavier@hotmail.com>
2023-06-27 14:28:54 -0300

    Módulos

--- Admin.js
+++ Admin.js
@@ -1,6 +1,6 @@
 import User from "./User.js";
 
-class Admin extends User {
+export default class Admin extends User {
   constructor(nome, email, nascimento, role = "admin", ativo = true) {
     super(nome, email, nascimento, role, ativo);
   }
@@ -9,9 +9,5 @@ class Admin extends User {
     return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`;
   }
 }
-
-const novoAdmin = new Admin("Rodrigo", "rg@r.com", "1998/03/07");
-console.log(novoAdmin.criarCurso("JS", "20"));
-console.log(novoAdmin.exibirInfos());
 
 // A palavra-chave super é usada para acessar o objeto pai de um objeto, em outros casos, é usada para acessar a classe pai de uma classe.
