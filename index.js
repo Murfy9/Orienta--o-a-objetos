166ac3d6e22da4b087049ac3c0203421fece5a2c
Pedro Xavier <pedroxxavier@hotmail.com>
2023-06-27 15:26:57 -0300

    Setters

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import User from "./User.js";
 import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
-const novoUser = new User("Marina", "m@m.com", "2022/08/26");
-console.log(novoUser.exibirInfos());
+// const novoUser = new User("Marina", "m@m.com", "2022/08/26");
+// console.log(novoUser.exibirInfos());
 
 const novoAdmin = new Admin("Rodrigo", "r@R.com", "2002/03/08");
+console.log(novoAdmin.nome);
+novoAdmin.nome = "Pedro";
 console.log(novoAdmin.nome);
