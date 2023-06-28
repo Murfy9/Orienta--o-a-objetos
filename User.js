b8f7010c1b466c63f6cd743d863f4cb85f01fa8a
Pedro Xavier <pedroxxavier@hotmail.com>
2023-06-28 14:18:57 -0300

    Comentários da aula4

--- User.js
+++ User.js
@@ -13,11 +13,14 @@ export default class User {
     this.#role = role || "estudante";
     this.#ativo = ativo;
   }
-  // Métodos
+
+  // Acessors: Getters e Setters.
 
   get nome() {
     return this.#nome;
   }
+
+  // Um getter é um tipo de função especial, que é somente leitura, ela apenas retorna uma propriedade da classe.
 
   get sobrenome() {
     return this.#sobrenome;
@@ -39,6 +42,8 @@ export default class User {
     return this.#ativo;
   }
 
+  // Setter são para alterar e validar essas alterações antes que elas sejam feitas nas propriedades que são privadas, de uma forma segura.
+
   set nome(novoNome) {
     if (novoNome === "") {
       throw new Error("Formato não válido!");
@@ -49,9 +54,15 @@ export default class User {
     this.#sobrenome = sobrenome;
   }
 
+  // Método
+
   exibirInfos() {
     return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.role}, ${this.ativo}`;
   }
 }
 
 // O construtor é um método especial para criar e inicializar um objeto criado a partir de uma classe.
+
+// Encapsulamento, é a ação de “esconder” os atributos de uma classe.
+
+// Atributos privados são o que chamamos quando ele (propriedades && métodos) não está acessível do lado de fora da classe.
